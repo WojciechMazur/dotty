f236ce92eafc24e4618cc8c4c356ec1a7e657180
Wojciech Mazur <wmazur@virtuslab.com>
2024-07-29 22:07:19 +0200

    Revert "Switch mapping of context bounds to using clauses in 3.6"

--- presentation-compiler/src/main/dotty/tools/pc/printer/ShortenedTypePrinter.scala
+++ presentation-compiler/src/main/dotty/tools/pc/printer/ShortenedTypePrinter.scala
@@ -296,7 +296,7 @@ class ShortenedTypePrinter(
     val (methodParams, extParams) = splitExtensionParamss(gsym)
     val paramss = methodParams ++ extParams
     lazy val implicitParams: List[Symbol] =
-      paramss.flatMap(params => params.filter(p => p.isOneOf(Flags.GivenOrImplicit)))
+      paramss.flatMap(params => params.filter(p => p.is(Flags.Implicit)))
 
     lazy val implicitEvidenceParams: Set[Symbol] =
       implicitParams
